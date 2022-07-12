2579d02fde12521f041bdc32d2e4f318be008d7d
unknown <sjjoseph106@gmail.com>
2022-07-12 20:24:56 +0900

    :lipstick: Demo 페이지 커스텀

--- src/components/demo/DemoContent.styled.tsx
+++ src/components/demo/DemoContent.styled.tsx
@@ -1,3 +1,21 @@
 import styled from "styled-components";
+import { media } from "styles/theme";
 
-export const Content = styled.div``;
+export const DemoContentLayout = styled.div`
+  text-align: center;
+  background: ${({ theme }) => theme.color.primary};
+  ${media.mobile} {
+    background: ${({ theme }) => theme.color.accent};
+  }
+  padding: 2rem;
+`;
+
+export const DemoContentText = styled.div`
+  color: ${({ theme }) => theme.color.accent};
+  ${media.mobile} {
+    color: ${({ theme }) => theme.color.primary};
+  }
+  font-size: ${(props: { [keys: string]: string }) =>
+    props.fontSize || "1.5rem"};
+  margin: ${(props: { [keys: string]: string }) => props.m};
+`;
--- src/components/demo/DemoContent.tsx
+++ src/components/demo/DemoContent.tsx
@@ -1,7 +1,23 @@
-import { Content } from "components/demo/DemoContent.styled";
+import * as S from "components/demo/DemoContent.styled";
+import { ReactSVG } from "assets/svg";
+import { rabbit } from "assets/img";
 
 const DemoContent = () => {
-  return <Content>CRA Boilerplate</Content>;
+  return (
+    <S.DemoContentLayout>
+      <S.DemoContentText fontSize={"2.4rem"} m={"1rem"}>
+        CRA Boilerplate
+      </S.DemoContentText>
+      <ReactSVG width="8rem" height="8rem" fill="#61DAFB" stroke="#ffffff" />
+      <S.DemoContentText className="pc-only" m={"1.5rem"}>
+        PC
+      </S.DemoContentText>
+      <S.DemoContentText className="mobile-only" m={"1.5rem"}>
+        Mobile
+      </S.DemoContentText>
+      <img src={rabbit} style={{ width: "25rem" }} alt="rabbit" />
+    </S.DemoContentLayout>
+  );
 };
 
 export default DemoContent;
