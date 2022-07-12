9bb58a624d12e8232772627391dd82c9025f76b1
unknown <sjjoseph106@gmail.com>
2022-07-12 16:20:53 +0900

    :lipstick: 기본 폰트 Pretendard 적용

--- src/styles/global-style.ts
+++ src/styles/global-style.ts
@@ -4,8 +4,11 @@ import { media } from "styles/theme";
 
 export const GlobalStyle = createGlobalStyle`
     ${reset}
-    * {
+    @import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.4/dist/web/static/pretendard.css");
+    html {
         font-size: 16px;
+        font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+        font-display: fallback;
     }
     ::-webkit-scrollbar {
         display: none;
