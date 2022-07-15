4abb5e5b0c17279f88d914544b0af54eac927c89
unknown <sjjoseph106@gmail.com>
2022-07-15 15:48:17 +0900

    :zap: lazy loading 적용

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,19 +1,24 @@
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { Main, Demo } from "pages";
+import { Suspense, lazy } from "react";
+
+const Main = lazy(() => import("pages/Main"));
+const Demo = lazy(() => import("pages/Demo"));
 
 const AppRouter = () => {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          {/* 404 */}
-          {/* <Route path="*" element={<NotFound />} /> */}
-          <Route path="/" element={<Main />} />
-          <Route path="demo" element={<Demo />} />
-          {/* Redirect */}
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
+        <Suspense fallback={<></>}>
+          <Routes>
+            {/* 404 */}
+            {/* <Route path="*" element={<NotFound />} /> */}
+            <Route path="/" element={<Main />} />
+            <Route path="demo" element={<Demo />} />
+            {/* Redirect */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </>
   );
--- src/pages/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as NotFound } from "pages/NotFound";
-export { default as Main } from "pages/Main";
-export { default as Demo } from "pages/Demo";
